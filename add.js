60c1ccbc229e8b508ab6faa4575a9e3602d61610
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-08 10:17:24 +0200

    js_studies

---
+++ add.js
@@ -0,0 +1,23 @@
+/* String Methods and Properties */
+
+//*********************************************** */
+
+/* length Property */
+
+/* var x = "Clarusway";
+var y = "";
+
+console.log (x.length);
+console.log (y.length); */
+
+//---------------------------------------------------
+/* 
+var myName = "Alex";
+myNameLength = myName.length;
+
+console.log(myNameLength); */
+
+//*********************************************** */
+
+/* concat() Method */
+
